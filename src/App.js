fad6e23de412865ebce9f89e5e6dead027813a77
Karumy <karumy_mcl@hotmail.com>
2024-06-24 23:01:59 -0600

    To do list app ready
    You can add new tasks in the input field, check as completed a task and also detele it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./HomePage.jsx";
+import HomePage from "./components/HomePage.jsx";
 import './App.css';
 
 function App() {
